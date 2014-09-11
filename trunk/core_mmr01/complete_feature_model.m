87fd31d24954d86b04224881158e7dfd3507e3bd
demost@gmail.com <demost@gmail.com@0c92af0f-b25d-3788-dd1b-2e6e17d9bd8d>
2014-09-11 11:23:21 +0000

    Initial import.

---
+++ trunk/core_mmr01/complete_feature_model.m
@@ -0,0 +1,8 @@
+MobileMedia : [Sorting] [SMS] Media [Favourite] [CopyMedia] :: _MobileMedia ;
+
+Media : [Music] Photo Video :: _Media ;
+
+Photo : [CapturePhoto] :: _Photo ;
+
+Video : [CaptureVideo] :: _Video ;
+
