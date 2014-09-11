9834d26ab305268b6462fbbda1d9cd2fd1bedd7e
demost <demost@gmail.com>
2014-09-11 02:03:55 +0000

    Initial import.

---
+++ argouml-app-v0.22/org/argouml/uml/diagram/sequence/ui/FigActivation.java
@@ -0,0 +1,17 @@
+//#if defined(SEQUENCEDIAGRAM)
+//@#$LPS-SEQUENCEDIAGRAM:GranularityType:Package
+
+package org.argouml.uml.diagram.sequence.ui;
+
+import org.tigris.gef.presentation.FigRect;
+
+public class FigActivation extends FigRect {
+    
+    private static final long serialVersionUID = -686782941711592971L;
+
+    FigActivation(int x, int y, int w, int h) {
+        super(x, y, w, h);
+        setFilled(true);
+    }
+}
+//#endif
