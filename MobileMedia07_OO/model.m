f76c0dcb93263304a6c7bc2928e1aadfe9270e88
demost <demost@gmail.com>
2014-09-11 01:33:09 +0000

    Initial import.

---
+++ MobileMedia07_OO/model.m
@@ -0,0 +1,4 @@
+MobileMedia : [Sorting] [Favourites] Media [CopyMedia] [Sms] [CapturePhoto] [CaptureVideo] [CaptureVideoOrCapturePhoto] [CopyMediaOrSms] [CopyMediaOrSmsOrCapturePhoto] [PhotoOrMusicOrVideo] [MusicOrVideo] [SmsOrCapturePhotoOrCaptureVideo] [PhotoAndMusicOrVideo] [CapturePhotoOrSms] :: _MobileMedia ;
+
+Media : [Music] [Video] [Photo] :: _Media ;
+
