a41c05b83951fe877b345ffc3b88d63b818dad6a
demost@gmail.com <demost@gmail.com@0c92af0f-b25d-3788-dd1b-2e6e17d9bd8d>
2014-09-11 11:38:56 +0000

    Initial import.

---
+++ trunk/sort_mmr03/src/lancs/mobilemedia/core/ui/MainUIMidlet.java
@@ -0,0 +1,86 @@
+package lancs.mobilemedia.core.ui;
+
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.Image;
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+
+import lancs.mobilemedia.core.ui.controller.BaseController;
+import lancs.mobilemedia.core.ui.controller.PhotoController;
+import lancs.mobilemedia.core.ui.datamodel.AlbumData;
+import lancs.mobilemedia.core.ui.datamodel.ImageData;
+import lancs.mobilemedia.core.ui.screens.AlbumListScreen;
+import lancs.mobilemedia.core.ui.screens.PhotoViewScreen;
+
+
+//Following are pre-processor statements to include the required
+//classes for device specific features. They must be commented out
+//if they aren't used, otherwise it will throw exceptions trying to
+//load classes that aren't available for a given platform.
+
+
+/* 
+ * @author trevor
+ *
+ * This is the main Midlet class for the core J2ME application
+ * It contains all the basic functionality that should be executable
+ * in any standard J2ME device that supports MIDP 1.0 or higher. 
+ * Any additional J2ME features for this application that are dependent
+ * upon a particular device (ie. optional or proprietary library) are
+ * de-coupled from the core application so they can be conditionally included
+ * depending on the target platform 
+ * 
+ * This Application provides a basic Photo Album interface that allows a user to view
+ * images on their mobile device. 
+ * */
+public class MainUIMidlet extends MIDlet {
+
+	//(m v C) Controller 
+	private BaseController rootController;
+
+	//Model (M v c)
+	private AlbumData model;
+
+	/**
+	 * Constructor -
+	 */
+	public MainUIMidlet() {
+	    //do nothing
+	}
+
+	/**
+	 * Start the MIDlet by creating new model and controller classes, and
+	 * initialize them as necessary
+	 */
+	public void startApp() throws MIDletStateChangeException {
+		model = new AlbumData();
+	    rootController = new BaseController(this, model);
+		//Only the first (last?) controller needs to be initialized (?)
+		rootController.init(model);
+		
+		//[Demostenes] - comandos adicionados para executar o handleCommand de cada controlador e outros comandos.
+		Command command = new Command("command",0,1);		
+		AlbumListScreen albumListScreen = new AlbumListScreen();
+		ImageData image = new ImageData(1, "parentAlbum", "image");
+		
+		rootController.commandAction(command, albumListScreen);
+				
+		PhotoController photoController = new PhotoController(image, rootController);
+		photoController.handleCommand(command, albumListScreen);				
+	}
+
+	/**
+	 * Pause the MIDlet
+	 * This method does nothing at the moment.
+	 */
+	public void pauseApp() {
+		//do nothing
+	}
+
+	/**
+	 * Destroy the MIDlet
+	 */
+	public void destroyApp(boolean unconditional) {
+		notifyDestroyed();
+	}
+}
